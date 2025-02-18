b8496a40d469e70234f6654b2a0101b50ace4e98
tunlu123 <talha.unlu@skyered-education.de>
2025-02-18 14:12:07 +0100

    added certificate

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Hier kannst du pro Skill einen Bildpfad hinterlegen,
 // z.B. 'linux': 'certificates/linux_cert.jpg'
 const skillImageMap = {
-  linux_Bash: "certificates/linux_bash_cert.jpg",
+  "linux & bash": "certificates/linux_bash_cert.jpg",
   // Weitere Zuordnungen möglich ...
   // Skills ohne Eintrag erhalten "Zertifikat ist in progress"
 };
